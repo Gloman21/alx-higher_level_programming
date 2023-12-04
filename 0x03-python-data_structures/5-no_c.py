a658c42c3448783ed7bfb23ce7bc1c9d6f6e504c
Bertin <irankundab21ertin@gmail.com>
2023-12-04 11:52:27 -0800

    fixed error

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
-    if my_string is None:
-        return None
     for elements in my_string:
-        if elements == "c" or elements == "C":
-        else:
+        if elements != "c" and elements != "C":
             new_string += elements
     return new_string
