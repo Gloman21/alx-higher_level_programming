de97899ecf226804ec40d1cd28ea367ff5d6b55e
Bertin <irankundab21ertin@gmail.com>
2024-01-05 09:32:18 -0800

    5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -60,3 +60,6 @@ class Rectangle:
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
         return (rect)
+
+    def __del__(self):
+        print("Bye rectangle...")
