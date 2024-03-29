2e99a2497dd4f0cc88117854d02bef7ff3d83da2
Bertin <irankundab21ertin@gmail.com>
2024-03-29 00:44:33 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,11 @@ import urllib.request as request
 
 if __name__ == "__main__":
     with request.urlopen("https://intranet.hbtn.io/status") as response:
-        if response.readable():
-            info = response.read()
+        info = response.read()
+        if info:
             print("Body response:")
             print("\t- type: {}".format(type(info)))
             print("\t- content: {}".format(info))
             print("\t- utf8 content: {}".format(info.decode("utf-8")))
+        else:
+            print("Response is empty")
